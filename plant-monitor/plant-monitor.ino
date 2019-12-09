f3c5a3526c99016e5e7dc9584dda451e7aba055d
Edis <edis.asotic@gmail.com>
2019-12-09 21:19:53 +0100

    :construction: testing basic button functionality

--- plant-monitor/plant-monitor.ino
+++ plant-monitor/plant-monitor.ino
@@ -1,24 +1,54 @@
 #include "WiFi.h"
 #include <TFT_eSPI.h>
+#include "xbm.h"
+#include <Button2.h>
 
-
+#define BUTTON_2        0
+#define BUTTON_1        35
+int statusShown = false;
 
 const char *SSID = "";
 const char *WiFiPassword = "";
-void setup() {
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextDatum(MC_DATUM);
-  tft.setRotation(0);
+TFT_eSPI tft = TFT_eSPI(135, 240); 
+Button2 btn1(BUTTON_1);
+Button2 btn2(BUTTON_2);
+
+
+
+void button_init()
+{
+
+    btn2.setPressedHandler([](Button2 & b) {
+        Serial.println("button pressed");
+        Serial.println(statusShown);
+        showStatus();
+    });
+
+    btn1.setPressedHandler([](Button2 & b){
+      Serial.println("button 2 pressed");
+      showHomeScreen();
+    });
   
+}
+
+
+void setup() {
+  tft.fillScreen(TFT_BLACK);
+  button_init();
   Serial.begin(115200);
-  ConnectToWiFi();
-  tft.begin();
+  tft.init();
+  showHomeScreen();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ button_loop();
+}
 
+void button_loop(){
+  btn1.loop();
+  btn2.loop();
+  
 }
 
 
@@ -43,4 +73,30 @@ void ConnectToWiFi(){
  
   Serial.print(F("Connected. My IP address is: "));
   Serial.println(WiFi.localIP());
+}
+
+
+void showStatus(){
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+
+ 
+  
+ int x = 0;
+ int y = 50;
+
+  tft.setTextSize(10);
+  tft.setRotation(1);
+  tft.drawString(":)",  tft.width() / 2, tft.height() / 2 );
+}
+
+void showHomeScreen(){
+  tft.setTextSize(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_BLUE, TFT_BLACK);
+  tft.setRotation(0);
+  tft.drawString("Press right button for status",0,0);
+  tft.drawString("status",0,10);
+
 }
